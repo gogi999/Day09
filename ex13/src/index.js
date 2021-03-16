1fa76947e57fa9756e37524da63580150af94417
gogi999 <g.petrovic9@gmail>
2021-03-16 21:44:22 +0100

    Updated ex13/src/index.js

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -12,8 +12,8 @@ function inverseWhile() {
    }
    return fiveNumbers;
 }
-// Only change code above this line
 
 console.log(inverseWhile());
+// Only change code above this line
 
 module.exports = inverseWhile;
